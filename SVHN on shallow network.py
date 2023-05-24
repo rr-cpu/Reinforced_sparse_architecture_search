31106d4110b4ad75a4d1b5b38d4a449f635204fa
rr <56760930+rr-cpu@users.noreply.github.com>
2023-05-24 17:49:40 +0530

    Update SVHN on shallow network.py

--- SVHN on shallow network.py
+++ SVHN on shallow network.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
+#code for training a shallow ann network on SVHN with sparsification
 
 
 import torch
@@ -12,47 +9,30 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-from torch.nn.utils import prune 
+from torch.nn.utils import prune                                        # To create binary masks for each layer that will act as prehook during training
 import pandas as pd
 import pickle
 from sparsity_module import Sparse_learn
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[ ]:
 
 
 class Svhn_Dataset:
-    def __init__(self):
+    def __init__(self, batch_size):
         self.transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485,0.456,0.0406], std=[0.229, 0.224, 0.225])])
         self.train_set = torchvision.datasets.SVHN(
             root='./data1', split='train', download=True, transform=self.transform)
         self.train_loader = torch.utils.data.DataLoader(
-            self.train_set, batch_size=128, shuffle=True, num_workers=2)
+            self.train_set, batch_size=batch_size, shuffle=True, num_workers=2)
 
         self.test_set = torchvision.datasets.SVHN(
             root='./data1', split='test', download=True, transform=self.transform)
         self.test_loader = torch.utils.data.DataLoader(
-            self.test_set, batch_size=128, shuffle=False, num_workers=2)
-
-    def imshow(self,img):
-        img = img / 2 + 0.5     # unnormalize
-        npimg = img.numpy()
-        plt.imshow(np.transpose(npimg, (1, 2, 0)))
-        plt.show()
+            self.test_set, batch_size=batch_size, shuffle=False, num_workers=2)
 
 
-    # get some random training images
-    def show_images(self):        
-        dataiter = iter(self.train_loader)
-        images, labels = next(dataiter)
-        print(images.shape,labels.shape)
-        # show images
-        self.imshow(torchvision.utils.make_grid(images))
-        # print labels
 
-
-# In[ ]:
 
 
 class NeuralNet(nn.Module):
@@ -70,14 +50,11 @@ class NeuralNet(nn.Module):
         return out
 
 
-# In[ ]:
 
 
 if __name__=="__main__":
     torch.manual_seed(0)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    dataset=Svhn_Dataset()
-    dataset.show_images()
     # architecture specs for shallow network
     input_size = 3072                              # 32x32x3----for SVHN dataset
     hidden_size = 1000                            
@@ -85,6 +62,8 @@ if __name__=="__main__":
     epochs=100
     learning_rate=0.1
     sparsity=0.9
+    batch_size=100
+    dataset=Svhn_Dataset(batch_size)
     model=NeuralNet(input_size, hidden_size, num_classes).to(device)
     sparse=Sparse_learn()
     result,measure_value=sparse.fit_sparse(model,dataset,epochs,learning_rate,device,sparsity)
@@ -97,7 +76,7 @@ if __name__=="__main__":
     plt.show()
 
 
-# In[ ]:
+
 
 
 
